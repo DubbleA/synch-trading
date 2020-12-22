641f0c4ed5bbd65c9d03003b3a00fc977d4e5be7
yams <40124776+DubbleA@users.noreply.github.com>
2020-12-22 16:26:31 -0500

    order
    can place order

--- trade.py
+++ trade.py
@@ -1,10 +1,32 @@
-import requests
+import requests, json
 from config import *
+HEADERS = {'APCA-API-KEY-ID': API_KEY, 'APCA-API-SECRET-KEY': SECRET_KEY}
+BASE_URL = "https://paper-api.alpaca.markets"
+ACCOUNT_URL = "{}/v2/account".format(BASE_URL)
+ORDERS_URL = "{}/v2/orders".format(BASE_URL)
 
-BASE_URL = "https://paper-api.alpaca.markets/"
+def get_account():
+    r = requests.get(ACCOUNT_URL, headers=HEADERS)
+    return json.loads(r.content)
 
-ACCOUNT_URL = "{}/V2/account".format(BASE_URL)
+def create_order(symbol, qty, side, type, time_in_force):
+    data = {
+        "symbol": symbol,
+        "qty": qty,
+        "side": side,
+        "type": type,
+        "time_in_force": time_in_force
+    }
+    r = requests.post(ORDERS_URL, json=data, headers=HEADERS)
+    return json.loads(r.content)
 
-r = requests.get(ACCOUNT_URL, headers={'APCA-API-KEY-ID': API_KEY, 'APCA-API-SECRET-KEY': SECRET_KEY})
+def get_orders():
+    r = requests.get(ORDERS_URL, headers=HEADERS)
+    return json.loads(r.content)
 
-print(r.content)
+
+response = create_order("AAPL", 100, "buy", "market", "gtc")
+response = create_order("MSFT", 1000, "buy", "market", "gtc")
+
+orders = get_orders()
+print(orders)
